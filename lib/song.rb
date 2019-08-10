040b060dc055737132817295510744fd25302ab6
Adam Schonely <aschonely@gmail.com>
2019-08-10 19:53:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,6 +48,11 @@ class Song
     split = filename.split(" - ")
     name = split[1].split(".")[0]
     artist_name = split[0]
+    
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
   end
   
   def self.create_from_filename(filename)
